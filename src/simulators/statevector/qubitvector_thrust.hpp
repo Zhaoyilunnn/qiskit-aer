d1c2f68c6a23dcc72c6cb450bfe60681bbd96558
Zhaoyilunnn <40353317+Zhaoyilunnn@users.noreply.github.com>
2021-04-01 08:59:00 -0400

    update

--- src/simulators/statevector/qubitvector_thrust.hpp
+++ src/simulators/statevector/qubitvector_thrust.hpp
@@ -2978,11 +2978,11 @@ double QubitVectorThrust<data_t>::apply_function(Function func,const reg_t &qubi
               for (i = iStream*nGPUBufferPerStream; i < iStream*nGPUBufferPerStream + nChunksOnGPU; i++) {
 //              std::cout << "Copying back to CPU ..." << std::endl;
                 m_Chunks[iPlace].Compression(i, chunkBits, 1, m_Streams[iStream]);
-                m_Chunks[iPlace].Put(m_Chunks[places[i]], m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits), i,
-                                     chunkBits, 1, m_Streams[iStream]);
-//                m_Chunks[iPlace].PutCompressed(m_Chunks[places[i]],
-//                                               m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits),
-//                                               chunkBits, m_Streams[iStream]);
+//                m_Chunks[iPlace].Put(m_Chunks[places[i]], m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits), i,
+//                                     chunkBits, 1, m_Streams[iStream]);
+                m_Chunks[iPlace].PutCompressed(m_Chunks[places[i]],
+                                               m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits),
+                                               chunkBits, m_Streams[iStream]);
               }
               // Switch stream
               iStream = (iStream + 1) % num_streams; // current stream
@@ -3020,11 +3020,11 @@ double QubitVectorThrust<data_t>::apply_function(Function func,const reg_t &qubi
           for (i = iStream*nGPUBufferPerStream; i < iStream*nGPUBufferPerStream + nChunksOnGPU; i++) {
 //            std::cout << "Copying back to CPU ..." << std::endl;
             m_Chunks[iPlace].Compression(i, chunkBits, 1, m_Streams[iStream]);
-            m_Chunks[iPlace].Put(m_Chunks[places[i]], m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits), i,
-                                 chunkBits, 1, m_Streams[iStream]);
-//            m_Chunks[iPlace].PutCompressed(m_Chunks[places[i]],
-//                                           m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits),
-//                                           chunkBits, m_Streams[iStream]);
+//            m_Chunks[iPlace].Put(m_Chunks[places[i]], m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits), i,
+//                                 chunkBits, 1, m_Streams[iStream]);
+            m_Chunks[iPlace].PutCompressed(m_Chunks[places[i]],
+                                           m_Chunks[places[i]].LocalChunkID(chunkIDs[i], chunkBits),
+                                           chunkBits, m_Streams[iStream]);
           }
         }
         // Update stream index
