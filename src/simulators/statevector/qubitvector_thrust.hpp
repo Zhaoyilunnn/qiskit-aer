a4bb62013a3c7d00f5fecbdefcfc0ae8251a4981
Zhaoyilunnn <40353317+Zhaoyilunnn@users.noreply.github.com>
2021-04-01 15:54:44 -0400

    update

--- src/simulators/statevector/qubitvector_thrust.hpp
+++ src/simulators/statevector/qubitvector_thrust.hpp
@@ -539,7 +539,7 @@ uint_t QubitVectorDeviceBuffer<data_t>::Compress(uint_t pos, uint_t size, cudaSt
 //      std::cout << m_Buffer[cc] << std::endl;
 //    }
 
-  if (cudaSuccess != cudaMemcpyToSymbolAsync(cbufd[iStream], &cbufl, sizeof(void *), 0, cudaMemcpyHostToDevice, stream))
+  if (cudaSuccess != cudaMemcpyToSymbolAsync(cbufd, &cbufl, sizeof(void *), iStream*sizeof(void*), cudaMemcpyHostToDevice, stream))
     fprintf(stderr, "copying of cbufl to device failed\n");
 
   CompressionKernel<<<BLOCKS, WARPSIZE*WARPS_BLOCK, 0, stream>>>(iStream);
