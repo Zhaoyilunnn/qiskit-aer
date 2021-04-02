5045c05c4db48cdb0129e2ef9e9e5e1e4e9d6309
Zhaoyilunnn <40353317+Zhaoyilunnn@users.noreply.github.com>
2021-04-02 05:02:44 -0400

    update

--- src/simulators/statevector/qubitvector_thrust.hpp
+++ src/simulators/statevector/qubitvector_thrust.hpp
@@ -907,8 +907,8 @@ int QubitVectorChunkContainer<data_t>::Allocate(uint_t size_in,uint_t bufferSize
       std::cout << "Allocating buffers on CPU for compression ..." << std::endl;
 
       m_pCut = new QubitVectorHostBuffer<int>(BLOCKS*WARPS_BLOCK);
-      m_pDbuf = new QubitVectorDeviceBuffer<uchar>(AER_MAX_GPU_BUFFERS*(m_doubles+1)/2*17);
-      m_pOff = new QubitVectorDeviceBuffer<int>(AER_MAX_GPU_BUFFERS*BLOCKS*WARPS_BLOCK);
+      m_pDbuf = new QubitVectorHostBuffer<uchar>(AER_MAX_GPU_BUFFERS*(m_doubles+1)/2*17);
+      m_pOff = new QubitVectorHostBuffer<int>(AER_MAX_GPU_BUFFERS*BLOCKS*WARPS_BLOCK);
 
       std::vector<int> cuts(BLOCKS*WARPS_BLOCK);
       int per = (m_doubles + BLOCKS*WARPS_BLOCK * WARPS_BLOCK - 1) / (BLOCKS*WARPS_BLOCK);
