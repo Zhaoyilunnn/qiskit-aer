32e63e57326fa8f29bf70dc0cfa5838d3deb35a2
Zhaoyilunnn <dilved1014@gmail.com>
2021-04-02 13:04:25 -0400

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python ./setup.py bdist_wheel -- -DAER_THRUST_BACKEND=CUDA -DCMAKE_CUDA_COMPILER=/usr/local/cuda-10.1/bin/nvcc -- -j32
+python ./setup.py bdist_wheel -- -DAER_THRUST_BACKEND=CUDA -DCMAKE_CUDA_COMPILER=/usr/local/cuda-10.1/bin/nvcc -DCUDA_NVCC_FLAGS="-rdc=true" -- -j32
 
 cd dist/ && pip uninstall * && pip install * && cd ..
