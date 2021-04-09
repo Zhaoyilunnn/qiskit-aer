1dda6cb3f9548c8b04644fe169297694536c7055
Zhaoyilunnn <40353317+Zhaoyilunnn@users.noreply.github.com>
2021-04-09 12:38:53 -0400

    update

--- src/simulators/statevector/qubitvector_thrust.hpp
+++ src/simulators/statevector/qubitvector_thrust.hpp
@@ -865,6 +865,7 @@ int QubitVectorChunkContainer<data_t>::Allocate(uint_t size_in,uint_t bufferSize
     else{
 #endif
       m_pChunks = new QubitVectorHostBuffer<thrust::complex<data_t>>(size);
+      m_pChunks->Fill(0);
       std::cout << "Allocated host size: " << size << std::endl;
 #ifdef AER_THRUST_CUDA
     }
